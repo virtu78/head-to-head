957d51ce8d58acc8080d53ebfbf5818b7d9d48ed
virtu78 <virtu78@yandex.ru>
2020-08-24 18:28:35 +0500

    deploy build

---
+++ dist/precache-manifest.6bc510cab0efd3b620932ce16841a079.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c1a518356a9e4f0ebf",
+    "url": "/headtohead/css/app.ca21e429.css"
+  },
+  {
+    "revision": "0831ddf4f96b1517ab54b0bd742b2a11",
+    "url": "/headtohead/img/preloader.0831ddf4.gif"
+  },
+  {
+    "revision": "51d4ded06d5916c725365d85f1351322",
+    "url": "/headtohead/index.html"
+  },
+  {
+    "revision": "70c1a518356a9e4f0ebf",
+    "url": "/headtohead/js/app.f1308c4a.js"
+  },
+  {
+    "revision": "7d5dbca078377b8f24f4",
+    "url": "/headtohead/js/chunk-vendors.11ef0b58.js"
+  },
+  {
+    "revision": "b9541da30d1d360b6a3c613475806984",
+    "url": "/headtohead/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/headtohead/robots.txt"
+  }
+]);
